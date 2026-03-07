5a0ee33cfc137a503a67691bd058288610f6f1a4
Heliomar Pena <penaheliomar@gmail.com>
2026-03-07 10:14:01 -0300

    feat: rotated image

--- boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -152,6 +152,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
 
+    lv_obj_set_style_transform_angle(art, 900, 0);  // 90°
+
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
